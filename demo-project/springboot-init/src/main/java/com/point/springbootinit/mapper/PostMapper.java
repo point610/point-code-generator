9ddada9053b7929e81a5f1a75fa5946fffb4fca9
point <2968448992@qq.com>
2024-01-23 12:58:41 +0800

    创建spring模板项目

---
+++ demo-project/springboot-init/src/main/java/com/point/springbootinit/mapper/PostMapper.java
@@ -0,0 +1,15 @@
+package com.point.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.point.springbootinit.model.entity.Post;
+
+/**
+ * 帖子数据库操作
+ */
+public interface PostMapper extends BaseMapper<Post> {
+
+}
+
+
+
+
