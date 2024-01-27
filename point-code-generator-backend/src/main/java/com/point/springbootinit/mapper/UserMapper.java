c2cfd60b32e6869c68c8e971e843894656ce40b7
point <2968448992@qq.com>
2024-01-27 17:13:42 +0800

    创建后端web项目

---
+++ point-code-generator-backend/src/main/java/com/point/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.point.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.point.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * 
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
