d8f69b71634892b552727742f086e49a67b53819
point <2968448992@qq.com>
2024-02-01 15:39:02 +0800

    增加平台在线使用代码生成器的功能

--- point-code-generator-frontend/src/services/backend/typings.d.ts
+++ point-code-generator-frontend/src/services/backend/typings.d.ts
@@ -210,6 +210,11 @@ declare namespace API {
     version?: string;
   };
 
+  type GeneratorUseRequest = {
+    dataModel?: Record<string, any>;
+    id?: number;
+  };
+
   type GeneratorVO = {
     author?: string;
     basePackage?: string;
